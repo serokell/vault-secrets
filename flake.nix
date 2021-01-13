ea954209fbcfda575adcb90d9cf6ca6a201fa328
Christian Höppner <christian.hoppner@serokell.io>
2021-01-13 18:19:54 +0000

    [OPS-1117] VM tests and CI

--- flake.nix
+++ flake.nix
@@ -1,9 +1,14 @@
 {
   description = "Serokell Vault Tooling";
 
-  outputs = { self }: {
+  inputs.nixpkgs.url = "github:serokell/nixpkgs";
+  inputs.nix-unstable.url = "github:nixos/nix";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-compat.url = "github:edolstra/flake-compat";
+  inputs.flake-compat.flake = false;
 
-    nixosModules.vault-secrets = import ./modules/vault-secrets.nix;
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    {
 
     overlay = final: prev: {
       vault-push-approles =
@@ -11,5 +16,27 @@
       vault-push-approle-envs =
         final.callPackage ./scripts/vault-push-approle-envs.nix { };
     };
-  };
+
+    nixosModules.vault-secrets = import ./modules/vault-secrets.nix;
+
+    } // (flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (pkgs) lib;
+        inherit (lib) mapAttrs;
+
+        nixMaster = inputs.nix-unstable.defaultPackage.${system};
+      in {
+        checks = let
+          tests = import ./tests/modules/all-tests.nix {
+            inherit pkgs system self;
+            callTest = t: t.test;
+            nixosPath = "${nixpkgs}/nixos";
+          };
+        in { inherit (tests) vault-secrets; };
+
+        legacyPackages = nixpkgs.legacyPackages.${system}.extend self.overlay;
+
+        devShell = pkgs.mkShell { buildInputs = [ nixMaster ]; };
+      }));
 }
