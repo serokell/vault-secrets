f85620b83cfba95aba7e6dd23d539ac7021de17a
con-f-use <con-f-use@gmx.net>
2024-05-03 11:43:20 +0200

    replace write_secrets.py by shell code
    Fixes #37

--- modules/vault-secrets.nix
+++ modules/vault-secrets.nix
@@ -13,7 +13,7 @@ in
   config = {
     systemd.services = mkMerge ([(flip mapAttrs' cfg.secrets (
       name: scfg: nameValuePair "${name}-secrets" {
-        path = with pkgs; [ getent jq vault-bin python3 ];
+        path = with pkgs; [ coreutils getent jq vault-bin ];
 
         partOf = map (n: "${n}.service") scfg.services;
         wantedBy = optional (scfg.services == []) "multi-user.target" ;
